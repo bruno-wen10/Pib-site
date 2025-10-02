3d7acc9a719a028f4420291ca4f87cc170f5614c
bruno-wen10 <bruno.wener.git@gmail.com>
2025-10-02 19:16:08 -0300

    fix: adiciona extensão .ts ao import da API

--- src/hooks/context/use-Eventos.tsx
+++ src/hooks/context/use-Eventos.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState, ReactNode, useCallback } from "react";
 import { Evento } from "@/types/events";
-import { fetchEventoById } from '@/services/Eventos-API/eventos-API'; // correção final.
+import { fetchEventoById } from '@/services/Eventos-API/eventos-API.ts';
+
 
 
 type EventosContextType = {
