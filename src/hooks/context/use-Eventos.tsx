4479210fb03898950372aacae8f6c7b3d45fecfa
bruno-wen10 <bruno.wener.git@gmail.com>
2025-10-02 19:13:08 -0300

    chore: finaliza rebase e confirma correção de caminho

--- src/hooks/context/use-Eventos.tsx
+++ src/hooks/context/use-Eventos.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, ReactNode, useCallback } from "react";
 import { Evento } from "@/types/events";
-import { fetchEventoById } from '@/services/Eventos-API/eventos-API';
+import { fetchEventoById } from '@/services/Eventos-API/eventos-API'; // correção final.
 
 
 type EventosContextType = {
